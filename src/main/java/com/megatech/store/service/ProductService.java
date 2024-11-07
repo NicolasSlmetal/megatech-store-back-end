8b3e3acc02d364f4f42eb651ed775083c1c60269
Nicolas Lima <nlimadasilvabrito@gmail.com>
2024-11-07 14:33:19 -0300

    feat: include validations for unique fields

--- src/main/java/com/megatech/store/service/ProductService.java
+++ src/main/java/com/megatech/store/service/ProductService.java
@@ -6,16 +6,18 @@ import com.megatech.store.dtos.products.DisplayProductDTO;
 import com.megatech.store.dtos.products.InsertProductDTO;
 import com.megatech.store.dtos.products.UpdateProductDTO;
 import com.megatech.store.exceptions.EntityNotFoundException;
+import com.megatech.store.exceptions.FieldConstraintViolationException;
 import com.megatech.store.model.ProductModel;
 import com.megatech.store.repository.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Consumer;
 
 @Service
 public class ProductService {
+
 
     private final ProductRepository productRepository;
 
@@ -33,7 +35,19 @@ public class ProductService {
         return product.orElseThrow(() -> new EntityNotFoundException("Product with id " + id + " not found"));
     }
 
+    public void validateIfNameNotExists(String name) {
+        if (productRepository.existsByName(name))
+            throw new FieldConstraintViolationException("Product with name " + name + " is invalid");
+    }
+
+    public void validateIfImageIsUsed(String imageName) {
+        if (productRepository.existsByImage(imageName))
+            throw new FieldConstraintViolationException("Product with image " + imageName + " is invalid");
+    }
+
     public DetailedProductDTO save(InsertProductDTO productDTO) {
+        validateIfNameNotExists(productDTO.name());
+        validateIfImageIsUsed(productDTO.image());
         Product product = new Product(productDTO);
         return new DetailedProductDTO(productRepository.save(new ProductModel(product)));
     }
@@ -41,10 +55,23 @@ public class ProductService {
     public DetailedProductDTO update(UpdateProductDTO productDTO, Long id) {
         Product savedProduct = new Product(productRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Product with id " + id + " not found")));
+
+        Product beforeUpdateProduct = savedProduct.clone();
         savedProduct.update(productDTO);
+        applyNecessaryValidations(beforeUpdateProduct, savedProduct);
+
         ProductModel updatedProductModel = new ProductModel(savedProduct);
         updatedProductModel.setId(id);
         return new DetailedProductDTO(productRepository.save(updatedProductModel));
+    }
+
+    private void applyNecessaryValidations(Product beforeUpdate, Product afterUpdate) {
+        if (!beforeUpdate.getName().equals(afterUpdate.getName())) {
+            validateIfNameNotExists(afterUpdate.getName());
+        }
+        if (!beforeUpdate.getImage().equals(afterUpdate.getImage())) {
+            validateIfImageIsUsed(afterUpdate.getImage());
+        }
     }
 
     public void delete(Long id){
