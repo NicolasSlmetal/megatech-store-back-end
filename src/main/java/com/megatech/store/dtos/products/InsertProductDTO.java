28e6f92a39d95cdd266dcccba69173bf5483dd63
Nicolas Lima <nlimadasilvabrito@gmail.com>
2024-11-07 00:25:22 -0300

    feat: add DTOs to insert and update products

---
+++ src/main/java/com/megatech/store/dtos/products/InsertProductDTO.java
@@ -0,0 +1,15 @@
+package com.megatech.store.dtos.products;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+
+public record InsertProductDTO(
+        @NotBlank(message = "name cannot be null or blank") String name,
+        @NotBlank(message = "manufacturer cannot be null or blank") String manufacturer,
+        @NotNull(message = "price cannot be null") @Positive(message = "price must be positive") Double price,
+        @NotNull(message = "stockQuantity cannot be null")
+        @PositiveOrZero(message = "stockQuantity must be zero or higher") Integer stockQuantity
+        ) {
+}
---
+++ src/main/java/com/megatech/store/dtos/products/UpdateProductDTO.java
@@ -0,0 +1,14 @@
+package com.megatech.store.dtos.products;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+
+public record UpdateProductDTO(
+        String name,
+        String manufacturer,
+        @Positive(message = "price must be positive") Double price,
+        @PositiveOrZero(message = "stockQuantity must be zero or higher") Integer stockQuantity
+) {
+}
