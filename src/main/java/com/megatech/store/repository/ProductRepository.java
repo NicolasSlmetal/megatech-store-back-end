f6510d48bf9c0b99cd569335abf8fc293ac2f782
Nicolas Lima <nlimadasilvabrito@gmail.com>
2024-11-07 14:32:59 -0300

    feat: add verifications of unique fields existence

--- src/main/java/com/megatech/store/repository/ProductRepository.java
+++ src/main/java/com/megatech/store/repository/ProductRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<ProductModel, Long> {
+
+    boolean existsByName(String name);
+    boolean existsByImage(String image);
 }
