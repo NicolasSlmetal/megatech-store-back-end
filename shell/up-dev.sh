843eddad7c5c1abbc3f2949614323e51d75c0fe7
Nicolas Lima <nlimadasilvabrito@gmail.com>
2024-11-08 19:43:09 -0300

    chore: include compilation command in shell up scripts

--- shell/up-dev.sh
+++ shell/up-dev.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+mvn clean install
 docker compose -f docker-compose.yaml -f docker-compose-dev.yaml up -d --build
--- shell/up-prod.sh
+++ shell/up-prod.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+mvn clean install
 docker compose -f docker-compose.yaml -f docker-compose-prod.yaml up -d --build
