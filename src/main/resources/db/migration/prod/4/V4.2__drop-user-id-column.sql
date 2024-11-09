c1a64b0a8d2c576967f56fd7ea4fe31b2e333807
Nicolas Lima <nlimadasilvabrito@gmail.com>
2024-11-09 18:56:59 -0300

    refact: drop user_id to use cst_id as foreign key of user

---
+++ src/main/resources/db/migration/prod/4/V4.2__drop-user-id-column.sql
@@ -0,0 +1,14 @@
+ALTER TABLE tb_costumer
+    RENAME TO tb_customer;
+
+ALTER TABLE tb_customer
+DROP CONSTRAINT fk_costumer_user;
+
+ALTER TABLE tb_customer
+    DROP COLUMN cst_user_id;
+
+ALTER TABLE tb_customer
+    ALTER COLUMN cst_id TYPE INT;
+
+ALTER TABLE tb_customer
+    ADD CONSTRAINT fk_customer_user FOREIGN KEY(cst_id) REFERENCES tb_user (user_id);
