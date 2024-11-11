c2e5157859740c450e19bad347079b315d88e596
Nicolas Lima <nlimadasilvabrito@gmail.com>
2024-11-11 17:00:34 -0300

    fix: change type of return to List instead of Optional

--- src/main/java/com/megatech/store/repository/AddressRepository.java
+++ src/main/java/com/megatech/store/repository/AddressRepository.java
@@ -4,12 +4,12 @@ import com.megatech.store.model.AddressModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface AddressRepository extends JpaRepository<AddressModel, Long> {
 
 
-    Optional<AddressModel>
+    List<AddressModel>
     findByStreetAndNumberAndCityAndStateAndZipcode(String street, Integer number, String city, String state, String zipcode);
 }
