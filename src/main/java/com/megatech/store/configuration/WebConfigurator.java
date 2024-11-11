f6b59e7a1893b4eac30bccdf5d31cf915a003267
Nicolas Lima <nlimadasilvabrito@gmail.com>
2024-11-11 18:52:04 -0300

    feat: add WebConfigurator with CORS options enabled

---
+++ src/main/java/com/megatech/store/configuration/WebConfigurator.java
@@ -0,0 +1,20 @@
+package com.megatech.store.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfigurator implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Authorization", "Content-Type", "Accept", "Origin", "X-Requested-With")
+                .allowCredentials(true);
+    }
+}
