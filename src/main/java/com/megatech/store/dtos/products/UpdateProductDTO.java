81ff588790d6ccdde395c15f0a1134768a57c2fe
Nicolas Lima <nlimadasilvabrito@gmail.com>
2024-11-11 11:24:21 -0300

    refact: remove unused imports

--- src/main/java/com/megatech/store/dtos/products/UpdateProductDTO.java
+++ src/main/java/com/megatech/store/dtos/products/UpdateProductDTO.java
@@ -1,7 +1,5 @@
 package com.megatech.store.dtos.products;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
 
