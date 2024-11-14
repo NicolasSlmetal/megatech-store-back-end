096a29623ac95763159b8a0059465f4c0df3540b
Nicolas Lima <nlimadasilvabrito@gmail.com>
2024-11-14 11:12:31 -0300

    feat: implement PurchaseDTO

---
+++ src/main/java/com/megatech/store/dtos/purchase/PurchaseDTO.java
@@ -0,0 +1,11 @@
+package com.megatech.store.dtos.purchase;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+public record PurchaseDTO(
+        Set<ProductQuantityDTO> productQuantities,
+        String customerName,
+        LocalDateTime date
+) {
+}
