cf635d0a6f4ba1eb1c3de4aa85ceb112d63dffda
Nicolas Lima <nlimadasilvabrito@gmail.com>
2024-11-14 11:11:41 -0300

    feat: implement PurchaseDependencyService to centralize the verification of Purchases dependencies

---
+++ src/main/java/com/megatech/store/service/PurchaseDependencyService.java
@@ -0,0 +1,60 @@
+package com.megatech.store.service;
+
+import com.megatech.store.domain.Customer;
+import com.megatech.store.domain.Product;
+import com.megatech.store.dtos.customer.InsertCustomerDTO;
+import com.megatech.store.dtos.products.InsertProductDTO;
+import com.megatech.store.dtos.purchase.CartItemDTO;
+import com.megatech.store.dtos.purchase.InsertPurchaseDTO;
+import com.megatech.store.exceptions.EntityNotFoundException;
+import com.megatech.store.factory.EntityModelFactory;
+import com.megatech.store.model.CustomerModel;
+import com.megatech.store.model.ProductQuantityMappingModel;
+import com.megatech.store.model.ProductModel;
+import com.megatech.store.repository.CustomerRepository;
+import com.megatech.store.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PurchaseDependencyService {
+
+    private final EntityModelFactory<Customer, CustomerModel, InsertCustomerDTO> customerFactory;
+    private final CustomerRepository customerRepository;
+    private final EntityModelFactory<Product, ProductModel, InsertProductDTO> productFactory;
+    private final ProductRepository productRepository;
+
+    public PurchaseDependencyService(EntityModelFactory<Customer, CustomerModel, InsertCustomerDTO> customerFactory, CustomerRepository customerRepository, ProductRepository productRepository, EntityModelFactory<Product, ProductModel, InsertProductDTO> productFactory) {
+        this.customerFactory = customerFactory;
+        this.customerRepository = customerRepository;
+        this.productFactory = productFactory;
+        this.productRepository = productRepository;
+    }
+
+    public Customer fetchCustomer(Long customerId) {
+        return customerFactory
+                .createEntityFromModel(customerRepository.findById(customerId)
+                        .orElseThrow(() -> new EntityNotFoundException("Customer not found")));
+    }
+
+    public List<Product> fetchProducts(InsertPurchaseDTO insertPurchaseDTO) {
+        List<Long> productsIds = insertPurchaseDTO.products().stream().map(CartItemDTO::productId).toList();
+
+        List<Product> products = productRepository
+                .findAllById(productsIds).stream()
+                .map(productFactory::createEntityFromModel).toList();
+
+        if (products.isEmpty() || products.size() != productsIds.size()) {
+            throw new EntityNotFoundException("Some product was not found");
+        }
+
+        return products;
+    }
+
+    public void decreaseStockQuantity(ProductQuantityMappingModel mappingProductQuantityModel) {
+        productRepository
+                .decreaseStockQuantity(mappingProductQuantityModel.getQuantity(), mappingProductQuantityModel.getProduct().getId());
+    }
+
+}
