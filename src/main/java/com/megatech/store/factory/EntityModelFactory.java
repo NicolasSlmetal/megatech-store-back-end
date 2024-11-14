3b12929bae0f98a4b15c08da3607db5e51d3b002
Nicolas Lima <nlimadasilvabrito@gmail.com>
2024-11-14 11:21:28 -0300

    fix: remove implementation of Entity in type T

--- src/main/java/com/megatech/store/factory/EntityModelFactory.java
+++ src/main/java/com/megatech/store/factory/EntityModelFactory.java
@@ -1,10 +1,9 @@
 package com.megatech.store.factory;
 
-import com.megatech.store.domain.Entity;
 import com.megatech.store.dtos.InputDTO;
 import com.megatech.store.model.Model;
 
-public interface EntityModelFactory <T extends Entity<? extends InputDTO>, M extends Model, DTO extends InputDTO> {
+public interface EntityModelFactory <T, M extends Model, DTO extends InputDTO> {
 
     T createEntityFromDTO(DTO dto);
     T createEntityFromModel(M model);
