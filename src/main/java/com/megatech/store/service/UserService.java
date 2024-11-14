7d25962dfade1421a5a3a4c08b33d43bbbe93453
Nicolas Lima <nlimadasilvabrito@gmail.com>
2024-11-14 11:17:54 -0300

    feat: add methods to control authentication and authorization

--- src/main/java/com/megatech/store/service/UserService.java
+++ src/main/java/com/megatech/store/service/UserService.java
@@ -1,6 +1,9 @@
 package com.megatech.store.service;
 
+import com.megatech.store.dtos.login.LoginDTO;
+import com.megatech.store.dtos.login.TokenDTO;
 import com.megatech.store.exceptions.InvalidUserFieldException;
+import com.megatech.store.model.UserModel;
 import com.megatech.store.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
@@ -8,9 +11,27 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final TokenService tokenService;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, TokenService tokenService) {
         this.userRepository = userRepository;
+        this.tokenService = tokenService;
+    }
+
+    public TokenDTO login(LoginDTO loginDTO) {
+        UserModel user = userRepository.findByEmail(loginDTO.email())
+                .orElseThrow(() -> new InvalidUserFieldException("Invalid email or password"));
+
+        if (!user.getPassword().equals(loginDTO.password())) {
+            throw new InvalidUserFieldException("Invalid email or password");
+        }
+
+        return new TokenDTO(tokenService.generateToken(user.getEmail(), user.getId()), user.getRole());
+    }
+
+    public void verifyAuth(String token) {
+        token = token.replace("Bearer ", "");
+        tokenService.verifyToken(token);
     }
 
     public void validateIfEmailExists(String email) {
