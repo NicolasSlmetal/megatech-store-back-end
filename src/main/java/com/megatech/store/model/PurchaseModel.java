ea652cff11f05944af8b3acf9c4a0b214e426899
Nicolas Lima <nlimadasilvabrito@gmail.com>
2024-11-14 11:10:06 -0300

    feat: implement Purchase model

---
+++ src/main/java/com/megatech/store/model/PurchaseModel.java
@@ -0,0 +1,36 @@
+package com.megatech.store.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Entity(name = "purchase")
+@Table(name ="tb_purchase")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PurchaseModel implements Model {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "pre_id")
+    private Long id;
+
+    @CreationTimestamp
+    @Column(name = "pre_date")
+    private LocalDateTime date;
+
+    @OneToMany(mappedBy = "purchase", cascade = CascadeType.MERGE)
+    private Set<ProductQuantityMappingModel> mappingProductQuantity;
+
+    @ManyToOne
+    @JoinColumn(name="pre_customer_id")
+    private CustomerModel customer;
+}
