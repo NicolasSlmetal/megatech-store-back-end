af3c9aaa27483c99d78b99d58525c0c056fb7ae2
Nicolas Lima <nlimadasilvabrito@gmail.com>
2024-11-16 17:14:28 -0300

    fix: add Not Null annotation to addCorsMappings parameter

--- src/main/java/com/megatech/store/configuration/WebConfigurator.java
+++ src/main/java/com/megatech/store/configuration/WebConfigurator.java
@@ -1,19 +1,18 @@
 package com.megatech.store.configuration;
 
+import jakarta.validation.constraints.NotNull;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfigurator implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(@NotNull CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("*")
+                .allowedOrigins("http://localhost:5500")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
 }
