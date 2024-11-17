24a7f672405159e2223ea7bf7f633308831a165f
Nicolas Lima <nlimadasilvabrito@gmail.com>
2024-11-17 00:20:23 -0300

    fix: implement equals and hash code of Id

--- src/main/java/com/megatech/store/dtos/purchase/CartItemDTO.java
+++ src/main/java/com/megatech/store/dtos/purchase/CartItemDTO.java
@@ -3,6 +3,8 @@ package com.megatech.store.dtos.purchase;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
+import java.util.Objects;
+
 public record CartItemDTO(
         @NotNull(message = "quantity of product cannot be null")
         @Positive(message = "quantity of product must be positive")
@@ -11,4 +13,15 @@ public record CartItemDTO(
         @Positive(message = "quantity of product must be positive")
         Long productId
 ) {
+        @Override
+        public boolean equals(Object o) {
+                if (o == null || getClass() != o.getClass()) return false;
+                CartItemDTO that = (CartItemDTO) o;
+                return Objects.equals(productId, that.productId);
+        }
+
+        @Override
+        public int hashCode() {
+                return Objects.hashCode(productId);
+        }
 }
