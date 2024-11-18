555cdabc97afce1196c4a23328d3fbd4e210c243
Nicolas Lima <nlimadasilvabrito@gmail.com>
2024-11-18 12:25:00 -0300

    feat: add redefineStockQuantity and findAll based on stockQuantity in ProductRepository

--- src/main/java/com/megatech/store/repository/ProductRepository.java
+++ src/main/java/com/megatech/store/repository/ProductRepository.java
@@ -2,14 +2,12 @@ package com.megatech.store.repository;
 
 import com.megatech.store.model.ProductModel;
 import com.megatech.store.projections.TotalValueInStockPerProduct;
-import com.megatech.store.projections.TotalValueSoldPerProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<ProductModel, Long> {
@@ -17,14 +15,25 @@ public interface ProductRepository extends JpaRepository<ProductModel, Long> {
     boolean existsByName(String name);
     boolean existsByImage(String image);
 
+    @Query(value = "SELECT * from tb_product p where p.prd_stock_quantity > 0", nativeQuery = true)
+    List<ProductModel> findAllByStockQuantityGreaterThanZero();
+
+    @Query(value = "SELECT * from tb_product p where p.prd_stock_quantity = 0", nativeQuery = true)
+    List<ProductModel> findAllByStockQuantityEqualZero();
+
+    @Modifying
+    @Query(value = "UPDATE tb_product SET prd_stock_quantity = 0 WHERE prd_id = ?1", nativeQuery = true)
+    void redefineStockQuantity(Long productId);
+
     @Query(value = "SELECT " +
             "p.prd_id as productId, " +
             "p.prd_name as productName, " +
+            "p.prd_entry_date as entryDate, " +
             "p.prd_price as productPrice, " +
             "p.prd_stock_quantity as quantity, " +
             "p.prd_stock_quantity * p.prd_price as totalValue " +
             "FROM " +
-            "tb_product p", nativeQuery = true)
+            "tb_product p WHERE p.prd_stock_quantity > 0", nativeQuery = true)
     List<TotalValueInStockPerProduct> getTotalValueInStockPerProduct();
     @Modifying
     @Query(value = "UPDATE tb_product SET prd_stock_quantity = prd_stock_quantity - ?1 WHERE prd_id = ?2", nativeQuery = true)
