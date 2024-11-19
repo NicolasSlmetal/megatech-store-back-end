d85c52fe39e0fed73a0314f51efb5538027626df
Nicolas Lima <nlimadasilvabrito@gmail.com>
2024-11-19 11:12:36 -0300

    feat: add authentication mapping for /me endpoints

--- src/main/java/com/megatech/store/configuration/SecurityConfigurator.java
+++ src/main/java/com/megatech/store/configuration/SecurityConfigurator.java
@@ -25,9 +25,9 @@ public class SecurityConfigurator {
         this.authenticationFilter = authenticationFilter;
     }
 
-    private static String PRODUCT_ENDPOINT = "/products";
-    private static String CUSTOMER_ENDPOINT = "/customers";
-    private static String PURCHASE_ENDPOINT = "/purchases";
+    private static final String PRODUCT_ENDPOINT = "/products";
+    private static final String CUSTOMER_ENDPOINT = "/customers";
+    private static final String PURCHASE_ENDPOINT = "/purchases";
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         return http.csrf(AbstractHttpConfigurer::disable)
@@ -43,6 +43,7 @@ public class SecurityConfigurator {
                     .requestMatchers(HttpMethod.POST, PRODUCT_ENDPOINT).hasAuthority(Role.ADMIN.toString())
                     .requestMatchers(HttpMethod.PUT, PRODUCT_ENDPOINT + "/**").hasAuthority(Role.ADMIN.toString())
                     .requestMatchers(HttpMethod.DELETE, PRODUCT_ENDPOINT + "/**").hasAuthority(Role.ADMIN.toString())
+                    .requestMatchers(CUSTOMER_ENDPOINT + "/me").authenticated()
                     .requestMatchers(CUSTOMER_ENDPOINT + "/**").permitAll()
                     .requestMatchers(PURCHASE_ENDPOINT + "/totalValue").hasAuthority(Role.ADMIN.toString())
                     .requestMatchers(PURCHASE_ENDPOINT + "/**").authenticated()
