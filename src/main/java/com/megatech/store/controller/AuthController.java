e7b705f2b8525bc18ddf3329e82dc1f8f99a8d41
Nicolas Lima <nlimadasilvabrito@gmail.com>
2024-11-18 22:56:40 -0300

    refact: remove /auth endpoint

--- src/main/java/com/megatech/store/controller/AuthController.java
+++ src/main/java/com/megatech/store/controller/AuthController.java
@@ -4,15 +4,13 @@ import com.megatech.store.dtos.login.LoginDTO;
 import com.megatech.store.dtos.login.TokenDTO;
 import com.megatech.store.service.UserService;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AuthController {
 
-    private UserService userService;
+    private final UserService userService;
 
     public AuthController(UserService userService) {
         this.userService = userService;
@@ -20,12 +18,7 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<TokenDTO> login(@RequestBody @Valid LoginDTO loginDTO) {
-        return ResponseEntity.ok(userService.login(loginDTO));
-    }
 
-    @GetMapping("/auth")
-    public ResponseEntity<Void> verifyAuth(@RequestHeader(HttpHeaders.AUTHORIZATION) String token) {
-        userService.verifyAuth(token);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(userService.login(loginDTO));
     }
 }
