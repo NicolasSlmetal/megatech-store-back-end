904f0d55f28d2a3bd915d1b4b75c4d9a5dc306d2
Nicolas Lima <nlimadasilvabrito@gmail.com>
2024-11-18 22:48:48 -0300

    feat: implement AuthenticationFilter to verify Authorization header before requests

---
+++ src/main/java/com/megatech/store/configuration/AuthenticationFilter.java
@@ -0,0 +1,59 @@
+package com.megatech.store.configuration;
+
+import com.megatech.store.exceptions.TokenErrorException;
+import com.megatech.store.security.UserAuthentication;
+import com.megatech.store.service.TokenService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class AuthenticationFilter extends OncePerRequestFilter {
+
+
+    private final TokenService tokenService;
+    public AuthenticationFilter(TokenService tokenService) {
+        this.tokenService = tokenService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = retrieveToken(request);
+
+        if (token != null) {
+            try {
+                UserDetails user = new UserAuthentication(tokenService.verifyToken(token));
+                Authentication authentication = getAuthenticationContext(user);
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } catch (TokenErrorException tokenErrorException) {
+                response.sendError(HttpServletResponse.SC_FORBIDDEN, tokenErrorException.getMessage());
+                return;
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private static UsernamePasswordAuthenticationToken getAuthenticationContext(UserDetails user) {
+        return new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword(), user.getAuthorities());
+    }
+
+    public String retrieveToken(HttpServletRequest request) {
+        String token = request.getHeader("Authorization");
+        if (token == null || token.isEmpty()) {
+            return null;
+        }
+        token = token.substring(7);
+        return token;
+    }
+}
