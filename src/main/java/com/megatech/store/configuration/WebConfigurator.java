02fe95de481728386bc1b2a324088083cb34b5c7
Nicolas Lima <nlimadasilvabrito@gmail.com>
2024-11-18 22:58:51 -0300

    refact: include loopback address on 5500 port on mapping

--- src/main/java/com/megatech/store/configuration/WebConfigurator.java
+++ src/main/java/com/megatech/store/configuration/WebConfigurator.java
@@ -11,7 +11,7 @@ public class WebConfigurator implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@NotNull CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5500")
+                .allowedOrigins("http://localhost:5500", "http://127.0.0.1:5500")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
